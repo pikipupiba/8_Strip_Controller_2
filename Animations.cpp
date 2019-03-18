89189f718dc4238044db6a599ed723d856c94d05
pikipupiba <St113tt)giub41891>
2019-03-18 12:45:21 -0500

    what do we think now

---
+++ Animations.cpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "Animations.h"
+
+
+
+Animations::Animations()
+{
+}
+
+Animations::~Animations()
+{
+}
+
+void Animations::UpdatePosition()
+{
+	hue += hueSpeed;
+	hueSpeed += hueAcceleration;
+
+	position += speed;
+	speed += acceleration;
+
+	switch (animationEndOfRange)
+	{
+	case Bounce:
+
+		if (position < rangeEnd && position > rangeStart)
+		{}
+		else if (position > rangeEnd)
+		{
+			position = rangeEnd;
+			speed = -speed;
+			if (acceleration > 0)
+			{
+				acceleration = -acceleration;
+			}
+		}
+		else if (position < rangeStart)
+		{
+			position = rangeStart;
+			speed = -speed;
+			if (acceleration < 0)
+			{
+				acceleration = -acceleration;
+			}
+		}
+		break;
+
+	case Continue:
+
+		if (position > rangeEnd)
+		{
+			position -= rangeSize;
+		}
+		else if (position < rangeStart)
+		{
+			position += rangeSize;
+		}
+		break;
+	}
+
+	end1 = position - featureSize / 2;
+	end2 = position + featureSize / 2;
+
+	if (end2 > rangeEnd)
+	{
+		end2 -= rangeSize;
+	}
+
+	if (end1 < rangeStart)
+	{
+		end1 += rangeSize;
+	}
+}
+
+void Animations::Update()
+{
+}
+
+void Animations::Draw()
+{
+}
