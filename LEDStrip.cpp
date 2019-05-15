ec60173eb21f278a3cd1996fea3c3adc66f58265
Joshua Sutter <pikipupiba@gmail.com>
2019-05-15 12:31:34 -0500

    Q3ETG

---
+++ LEDStrip.cpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "LEDStrip.h"
+#include "palettes.h"
+
+LEDStrip::LEDStrip(CRGBSet* leds)
+{
+	D(startTime("LEDStrip::LEDStrip(CRGBSet leds)");)
+
+	vars = {
+		leds,		// CRGBSet
+		leds->len,	// numLeds
+		2,			// curPattern
+		120,		// brightness
+		0,			// hue
+		1,			// hueSpeed
+		0,			// hue2
+		0,			// position
+		1,			// speed
+		10,			// size
+		0,			// clockOffset
+		palettes[0],// palette
+		palettes[0]	// tPalette
+	};
+
+	power = true;
+
+	stripAutoplay = false;
+	stripAutoplayDuration = 10000;
+
+	D(endTime("LEDStrip::LEDStrip(CRGBSet leds)");)
+}
+
+LEDStrip::~LEDStrip()
+{
+}
+
+void LEDStrip::UpdateStrip()
+{
+	D(startTime("LEDStrip::UpdateStrip()");)
+
+	UpdatePatternVars();
+
+	patterns[vars.curPattern](vars);
+
+	if (stripAutoplay && (millis() > stripAutoPlayTimeout)) {
+		NextPattern();
+		stripAutoPlayTimeout = millis() + (stripAutoplayDuration);
+	}
+
+	D(endTime("LEDStrip::UpdateStrip()");)
+}
+
+void LEDStrip::UpdatePatternVars()
+{
+	D(startTime("LEDStrip::UpdatePatternVars()");)
+
+		vars.hue += vars.hueSpeed;
+		vars.position += vars.speed;
+
+	D(endTime("LEDStrip::UpdatePatternVars()");)
+}
+
+// Reset the timeouts for the strip when autoplay is turned on or after setup.
+void LEDStrip::ResetTimeouts()
+{
+	D(startTime("LEDStrip::ResetTimeouts()");)
+
+	stripAutoPlayTimeout = millis() + (stripAutoplayDuration);
+
+	D(endTime("LEDStrip::ResetTimeouts()");)
+}
+
+void LEDStrip::NextPattern()
+{
+	D(startTime("LEDStrip::NextPattern()");)
+
+	vars.curPattern = (vars.curPattern + 1) % patternCount;
+
+	D(endTime("LEDStrip::NextPattern()");)
+}
+
+void LEDStrip::PrintStripInfo()
+{
+	
+}
